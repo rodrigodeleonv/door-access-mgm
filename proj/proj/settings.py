5356785b4b67e56effb6f489336dd368e4ef1ae9
Rodrigo de León <rodrigodeleonv@gmail.com>
2024-05-07 17:33:23 -0600

    chore: add api TokenAuthentication
    migrate after configure it

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # External
     'django_extensions',
     'rest_framework',
+    'rest_framework.authtoken',
     # Internal apps
     'users',
     'access_control',
@@ -153,7 +154,12 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        # # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50
