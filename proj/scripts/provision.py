d54cdafefeb6973ff23582dfd9d54b8bc0b2d054
Rodrigo de León <rodrigodeleonv@gmail.com>
2024-05-08 14:04:19 -0600

    chore: create provisioning script

---
+++ proj/scripts/provision.py
@@ -0,0 +1,56 @@
+"""Provisioning script.
+
+.. code-block:: bash
+
+   python proj/manage.py runscript provision
+"""
+
+import json
+import logging
+
+from decouple import config
+from rest_framework.authtoken.models import Token
+
+from users.models import User
+from access_control.models import RFIDTag
+
+logger = logging.getLogger(__name__)
+
+
+def provision_superuser() -> None:
+    """Provision Superuser if not exists."""
+    exists_superuser = User.objects.filter(is_superuser=True).exists()
+    if not exists_superuser:
+        logger.info("Creating superuser")
+        username = config("PROVISION_SUPERUSER_EMAIL")
+        password = config("PROVISION_SUPERUSER_PASSWORD")
+        super_u = User.objects.create_superuser(
+            email=username, password=password, first_name="Super", last_name="User"
+        )
+        # Create token for superuser
+        Token.objects.create(user=super_u)
+    else:
+        logger.info("Do not create superuser, already exists")
+
+
+def provison_tags() -> None:
+    """Provision Tags from JSON file."""
+    logger.info("Provision RFID Tags")
+    with open("tags.json") as f:
+        tags = json.load(f)
+    for tag in tags["tags"]:
+        tag_id = tag["tag_id"].strip()
+        _, created = RFIDTag.objects.get_or_create(tag_id=tag_id)
+        if created:
+            logger.info(f"Tag {tag_id} created")
+
+
+def run():
+    try:
+        provision_superuser()
+    except Exception:
+        logger.exception("Provision superuser failed")
+    try:
+        provison_tags()
+    except Exception:
+        logger.exception("Provision tags failed")
